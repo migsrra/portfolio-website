df3bfff8f45c2c7cb796060ec8066ebaf3d0a12c
migsrra <miguel.serra.estrada@gmail.com>
2024-09-13 17:00:19 -0400

    feature(site header): Completed initial implementation of site header

--- my-site/src/components/header/styles/HeaderWrapper.js
+++ my-site/src/components/header/styles/HeaderWrapper.js
@@ -1,7 +1,40 @@
 import styled from "styled-components";
+import Constants from "../../../utils/constants";
 
 const HeaderWrapper = styled.header`
+  .header{
+    font-family: 'Roboto Slab';
+    font-weight: normal;
+    font-weight: 500;
+    font-size: 16px;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 30px 10%;
 
+    a {
+      transition: all 0.3s ease 0s;
+    }
+    a:hover{
+      color: ${Constants.textHover};
+    }
+    .active{
+      color: ${Constants.textHover};
+    }
+    
+  } 
+  .nav_links {
+    list-style: none;
+  }
+  .nav_links li {
+    display: inline-block;
+    padding: 0px 20px;
+  }
+  
 `
 
 export default HeaderWrapper;
---
+++ my-site/src/utils/constants.js
@@ -0,0 +1,9 @@
+const Constants = {
+  textColor: "#E2E2B6",
+  textHover: "#6EACDA",
+  gray: '#24252A',
+  backgroundColor: '#021526',
+  headerColor: '#a5957c'
+};
+
+export default Constants;
